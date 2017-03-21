eb6dc19e9b1699a1cd8a39da1c909b6c3bc813f1
cmudevops <lenbass@cmu.edu>
2017-03-21 11:36:45 -0400

    Update knife.rb

--- knife.rb
+++ knife.rb
@@ -5,6 +5,6 @@ node_name                "admin"
 client_key               "#{current_dir}/admin.pem"
 validation_client_name   "cmudevops-validator"
 validation_key           "#{current_dir}/cmudevops-validator.pem"
-chef_server_url          "https://127.0.01/organizations/digitalocean"
+chef_server_url          "https://127.0.0.1/organizations/cmudevops"
 syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
 cookbook_path            ["#{current_dir}/../cookbooks"]
